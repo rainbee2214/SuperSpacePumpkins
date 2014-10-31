7253a47efd2fe6e4e50a379c425ebd390d540a42
rainbee2214 <rainbee2214@gmail.com>
2014-10-31 18:29:38 -0300

    Assets and setup

---
+++ SuperSpacePumpkin/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour 
+{
+	public float distanceFromPlanet;
+	public float phi;
+	public float theta;
+	public float speed;
+	public float bound = 3;
+
+	public Transform target;
+	
+	Vector3 origin = new Vector3(0,0,0);
+	Vector3 position;
+	
+	void Start()
+	{
+		position = origin;
+		
+		//Convert theta and phi to radians
+		theta = theta * (Mathf.PI / 180);
+		phi = phi * (Mathf.PI / 180);
+
+	}
+	
+	void Update()
+	{
+		position.x = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Cos(theta);
+		position.y = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Sin(theta);
+		position.z = distanceFromPlanet*Mathf.Cos(phi);
+		
+		transform.position = position;
+		transform.LookAt(target);
+
+		phi = phi + speed;
+		if (phi >= bound)
+		{
+			phi = 0;
+		}
+	}
+	
+}
