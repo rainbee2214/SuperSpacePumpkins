f5ebcc47462dba21c2e5d992273b93308ded7e1d
rainbee2214 <rainbee2214@gmail.com>
2014-10-31 19:45:34 -0300

    Automated the opening screen motions and randomized all the input parameters.

--- SuperSpacePumpkin/Assets/Scripts/CameraPosition.cs
+++ SuperSpacePumpkin/Assets/Scripts/CameraPosition.cs
@@ -6,18 +6,29 @@ public class CameraPosition : MonoBehaviour
 	public float distanceFromPlanet;
 	public float phi;
 	public float theta;
-	public float speed;
-	public float bound = 3;
+	public float phiSpeed;
+	public float thetaSpeed;
+	public float bound = 3f;
 
 	public Transform target;
 	
 	Vector3 origin = new Vector3(0,0,0);
 	Vector3 position;
-	
+
+	bool pushingCamera = true;
+	public float planetPushSpeed = 0.1f;
+	public float targetDistance = 10f;
+
+	public Material[] boxes;
 	void Start()
 	{
+		int i = Random.Range (0, boxes.Length);
+		Debug.Log (i);
+		Skybox skybox = gameObject.GetComponent<Skybox>(); 
+		skybox.material = boxes[i];
 		position = origin;
-		
+
+		theta = Random.Range(-180, 180);
 		//Convert theta and phi to radians
 		theta = theta * (Mathf.PI / 180);
 		phi = phi * (Mathf.PI / 180);
@@ -26,6 +37,15 @@ public class CameraPosition : MonoBehaviour
 	
 	void Update()
 	{
+		if (pushingCamera)
+		{
+			pushCameraOut();
+			if (distanceFromPlanet >= targetDistance)
+			{
+				pushingCamera = false;
+			}
+		}
+
 		position.x = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Cos(theta);
 		position.y = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Sin(theta);
 		position.z = distanceFromPlanet*Mathf.Cos(phi);
@@ -33,11 +53,22 @@ public class CameraPosition : MonoBehaviour
 		transform.position = position;
 		transform.LookAt(target);
 
-		phi = phi + speed;
+		phi = phi + phiSpeed;
 		if (phi >= bound)
 		{
-			phi = 0;
+			//phi = 0;
 		}
+
+		theta = theta + thetaSpeed;
+		if (theta >= bound)
+		{
+			//phi = 0;
+		}
+	}
+
+	void pushCameraOut() 
+	{
+		distanceFromPlanet += planetPushSpeed;
 	}
 	
 }
---
+++ SuperSpacePumpkin/Assets/Scripts/OpeningPumpkinPosition.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningPumpkinPosition : MonoBehaviour 
+{
+	float distanceFromPlanet;
+	float phi;
+	float theta;
+	float phiSpeed;
+	public float bound = 3;
+	
+	public Transform target;
+
+	Vector3 position;
+	
+	void Start()
+	{		
+		//Generate distance from planet, phi, theta, phiSpeed
+		distanceFromPlanet = Random.Range(2.5f, 4.5f);
+		phi = Random.Range(2.5f, 4.5f);
+		theta = Random.Range(-180, 180);
+		phiSpeed = Random.Range(0.005f, 0.025f);
+		//Convert theta and phi to radians
+		theta = theta * (Mathf.PI / 180);
+		phi = phi * (Mathf.PI / 180);
+		
+	}
+	
+	void Update()
+	{
+		position.x = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Cos(theta);
+		position.y = distanceFromPlanet*Mathf.Sin(phi)*Mathf.Sin(theta);
+		position.z = distanceFromPlanet*Mathf.Cos(phi);
+		
+		transform.position = position;
+		transform.LookAt(target);
+		
+		phi = phi + phiSpeed;
+		if (phi >= bound)
+		{
+			phi = 0;
+		}
+
+	}
+	
+}
+
